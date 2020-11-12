a26d81bc3c75cae5b5c254ab7338d260e406d5fe
victoriest <>
2020-11-12 10:45:04 +0800

    copy yolo tags with a new tag

--- deep-learning-playgroud-tf2/data_pretreatment/copy_yolo_tags_with_a_new_tag.py
+++ deep-learning-playgroud-tf2/data_pretreatment/copy_yolo_tags_with_a_new_tag.py
@@ -1,21 +1,24 @@
 import os
 
 
-def generate_yolov4_manipast():
-    f = open('train.txt', 'w')
-    g = os.walk("E:/_dataset/zyb_opt_mark_data/yolo")
-    lines = []
-
+def copy_yolo_tags_with_a_new_tag():
+    g = os.walk("D:/_data/zyb_data_frame")
     for path, dir_list, file_list in g:
         for file_name in file_list:
-            # print(os.path.join(path, file_name), os.path.splitext(file_name))
-            rf = open(os.path.join(path, file_name), "r")
+            if os.path.splitext(file_name)[1] != '.txt':
+                continue
+            rf = open(os.path.join(path, file_name), "r", encoding='UTF-8')
             lines = rf.readlines()
             new_lines = []
             for l in lines:
                 words = l.split(' ')
-                words[0] = '7'
-                new_lines.append(" ".join(words))
+                if words[0] == '3' or words[0] == '4' or words[0] == '5':
+                    words[0] = '2'
+                    new_lines.append(" ".join(words))
+                elif words[0] == '7' or words[0] == '8' or words[0] == '9' or words[0] == '10' or words[0] == '11':
+                    words[0] = '6'
+                    new_lines.append(" ".join(words))
+
             lines.extend(new_lines)
             print(len(new_lines), len(lines), new_lines)
             rf.close()
@@ -24,4 +27,4 @@ def generate_yolov4_manipast():
             wf.close()
 
 
-generate_yolov4_manipast()
+copy_yolo_tags_with_a_new_tag()
