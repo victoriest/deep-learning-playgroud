abcff7900674eb5c7acd7916440c84459276f9f9
victoriest <heest0205@gmail.com>
2020-12-02 11:44:36 +0800

    p站爬虫

--- deep-learning-playgroud-tf2/data_pretreatment/pixiv_spider.py
+++ deep-learning-playgroud-tf2/data_pretreatment/pixiv_spider.py
@@ -1,150 +1,159 @@
-#!/usr/bin/env python3
-# -*- encoding: utf-8 -*-
-# Author:  MoeClub.org
-# https://github.com/MoeClub/Pixiv
-
-from urllib import request
-import threading
-import queue
-import re
+import json
 import os
+import requests
+from bs4 import BeautifulSoup as bs
+import threading
 
 
-class pixiv:
-    def __init__(self):
-        self.folder = 'PixivImage'
-        self.web_coding = 'utf-8'
-        self.root = os.path.dirname(os.path.abspath(__file__))
-        self.DefaultHeader = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:52.0) Gecko/20100101 Firefox/52.0",
-            "Accept": "*/*",
-            "Accept-Language": "zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3",
-            "Accept-Encoding": "",
-            "Connection": "keep-alive",
+class Pixiv:
+
+    def __init__(self, moder, pn):
+        self.pixiv_username = ''
+        self.pixiv_password = ''
+
+        self.accounts_url = 'https://accounts.pixiv.net/login?return_to=https%3A%2F%2Fwww.pixiv.net%2F&lang=zh&source=pc&view_type=page'  # 登录界面连接
+        self.login_url = 'https://accounts.pixiv.net/api/login?lang=zh'  # 登录URL
+        self.post = []  # 获取登录所需的随机cookie
+
+        # 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36' Linux请求头
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36',
+            'referer': 'https://accounts.pixiv.net/login?lang=zh&source=pc&view_type=page&ref=wwwtop_accounts_index'
+            # 校验码，不添加会被反扒403错误
         }
-        self.data_low = []
-        self.num = 0
+        self.list_id = []  # id列表
+        self.list_url = []  # 排行榜url列表
+        self.url_rank = 'https://www.pixiv.net/ranking.php?'  # 排行榜url
+        self.params_rank = {  # 排行榜url接口
+            'mode': moder,
+            'content': 'illust',
+            'p': pn,
+            'format': 'json'
+        }
 
-    def _http(self, url, headers, Obj=False):
-        res = request.urlopen(request.Request(url, headers=headers, method='GET'))
-        if Obj:
-            return res
-        else:
-            return res.read().decode(self.web_coding, "ignore")
+        self.i = 1  # 图片id
+        self.glock = threading.Lock()  # 锁
 
-    def data_image(self, url_id):
-        _header = self.DefaultHeader.copy()
-        _header["Referer"] = "https://www.pixiv.net/member_illust.php?mode=medium&illust_id={}".format(url_id)
-        _url_data = "https://www.pixiv.net/touch/ajax/illust/details?illust_id={}".format(url_id)
-        _data_details = self._http(_url_data, _header)
-        data_url = self.sort_data(re.findall('"url_big":"[^"]*"', _data_details))
-        data_uid = str(str(str(re.findall('"user_id":"[^"]*"', _data_details)[0]).split(':', 1)[-1]).strip('"'))
-        return data_url, _header, data_uid
+    def login(self):  # 模拟登陆
+        se = requests.session()
+        pixiv_key_html = se.get(self.accounts_url, headers=self.headers).text
 
-    def sort_data(self, data):
-        _data = []
-        for item in data:
-            if item not in _data:
-                _data.append(item)
-        return [str(str(item).replace('\\', '').split(':', 1)[-1]).strip('"') for item in _data]
+        pixiv_key_soup = bs(pixiv_key_html, 'lxml')
+        self.post = pixiv_key_soup.find('input')['value']
+        # print(self.post)
+        data = {
+            'pixiv_id': self.pixiv_username,
+            'password': self.pixiv_password,
+            'post_key': self.post,
+            "ref": "wwwtop_accounts_index",
+            "source": "pc",
+            'return_to': 'https://www.pixiv.net/'
+        }
 
-    def get_item(self, UserID=None):
-        if not UserID:
-            UserID = 'https://www.pixiv.net/ranking.php?mode=male'
-        if '://' in str(UserID):
-            Mode_ID = False
-        else:
-            Mode_ID = True
-        if Mode_ID:
-            _url = "https://www.pixiv.net/ajax/user/{}/profile/all".format(str(UserID))
-            page = self._http(_url, self.DefaultHeader, True)
-            if page.code != 200:
-                raise Exception("Pixiv Page:", page.code)
-            _data = re.findall('"[0-9]+":null', page.read().decode(self.web_coding, "ignore"))
-            self.data_low = [str(str(item).split(":")[0]).strip('"') for item in _data if ':null' in str(item)]
-        else:
-            page = self._http(UserID, self.DefaultHeader, True)
-            if page.code != 200:
-                raise Exception("Pixiv Page:", page.code)
-            _data = re.findall('data-src="[^"]*"', page.read().decode(self.web_coding, "ignore"))
-            self.data_low = [str(str(str(str(str(item).split("=", 1)[-1]).strip('"')).rsplit('/', 1)[-1]).split('_')[0])
-                             for item in _data if '/img-master/img/' in str(item)]
-        self.fliter_item()
+        dare = se.post(self.login_url, data=data, headers=self.headers).text  # 登录
+        da = json.loads(dare)
+        print(da)
+        se.close()
 
-    def fliter_item(self):
-        folder = os.path.join(self.root, self.folder)
-        if not os.path.exists(folder):
-            return None
-        _split = "_"
-        _exist = {}.fromkeys([str(str(item).split(_split)[1]) for item in os.listdir(folder) if _split in item]).keys()
-        print("Exist Item:", len(_exist))
-        for _item in self.data_low.copy():
-            if _item in _exist:
-                self.data_low.remove(_item)
+        # Pixiv.test(self)
+        # Pixiv.url_get(self)
 
-    def get_data_by_item(self, item):
-        data = self.data_image(item)
-        for data_url in data[0]:
-            image = self._http(data_url, data[1], True)
-            if image.code != 200:
-                raise Exception("Pixiv Image: [{} | {}]".format(image.code, data[0]))
-            self.write(str("{}_{}").format(str(data[2]), str(str(data_url).rsplit('/', 1)[-1])), image.read())
+    def id_get(self):  # 获取rank作品id
+        if self.params_rank['mode'] == '1':
+            self.params_rank['mode'] = 'daily'  # 日排行
+            self.headers['referer'] = 'https://www.pixiv.net/ranking.php?mode=daily&content=illust'
 
-    def get_data(self, data_list=None):
-        if not data_list:
-            data_list = self.data_low
-        for item in data_list:
-            self.get_data_by_item(item)
-        print("\nTotal Image: ", self.num)
+        elif self.params_rank['mode'] == '2':
+            self.params_rank['mode'] = 'weekly'  # 周排行
+            self.headers['referer'] = 'https://www.pixiv.net/ranking.php?mode=weekly&content=illust'
 
-    def write(self, name, data):
-        folder = os.path.join(self.root, self.folder)
-        if not os.path.exists(folder):
-            os.mkdir(folder)
-        file = os.path.join(folder, str(name))
-        fp = open(file, 'wb')
-        fp.write(data)
-        fp.close()
-        self.num += 1
-        print("Pixiv Image: [ OK | {} ]".format(file))
+        elif self.params_rank['mode'] == '3':
+            self.params_rank['mode'] = 'monthly'  # 月排行
+            self.headers['referer'] = 'https://www.pixiv.net/ranking.php?mode=monthly&content=illust'
 
-    def add_queue(self, _queue, data_list=None):
-        for item in data_list:
-            _item = str(item).strip()
-            if item and _item:
-                _queue.put(_item)
+        for u in range(int(self.params_rank['p'])):
+            self.params_rank['p'] = str(u + 1)
+            # print(self.params_rank)
+            url_get = requests.get(self.url_rank, headers=self.headers, params=self.params_rank).text
+            url_json = json.loads(url_get)
+            # print(url_json)
 
-    def multi_data(self, data_list=None, max=25):
-        if not data_list:
-            data_list = self.data_low
-        print("New Item:", len(data_list))
-        _threads = []
-        _queue = queue.Queue(maxsize=max)
-        task_main = threading.Thread(target=self.add_queue, args=(_queue, data_list))
-        task_main.setName("TaskMain")
-        task_main.setDaemon(True)
-        task_main.start()
-        while _queue.qsize() > 0:
-            if len(_threads) >= max:
-                for _item in _threads.copy():
-                    if not _item.is_alive():
-                        _threads.remove(_item)
-                continue
-            item = _queue.get()
-            task = threading.Thread(target=self.get_data_by_item, args=(item,))
-            task.setDaemon(True)
-            task.start()
-            _threads.append(task)
-        for _task in _threads:
-            _task.join()
-        print("\nTotal Image: ", self.num)
+            for dict1 in url_json['contents']:  # 获取图片id
+                self.list_id.append(dict1['illust_id'])
+            # Pixiv.test(self)
+
+    def url_get(self):  # 多线程获取url
+        while True:
+            self.glock.acquire()  # 加锁
+            if len(self.list_id) == 0:
+                self.glock.release()  # 释放锁
+                break
+            else:
+                ID = self.list_id.pop(0)  # 提取列表第一个URL并删除
+                self.glock.release()  # 释放锁
+                test1 = 'https://www.pixiv.net/ajax/illust/' + str(ID) + '/pages?lang=zh'  # 查询真实ID包
+                url_text = requests.get(test1, headers=self.headers).text
+                url_testjson = json.loads(url_text)
+
+                for dict2 in url_testjson['body']:  # 获取url
+                    self.list_url.append(dict2['urls']['original'])
+                    url = dict2['urls']['original']
+                    print(f'获取链接:{url}')
+
+    # def test(self):   # 测试保存
+    #     j = 1
+    #     for i in self.dict_rank:
+    #         self.headers['referer'] = 'https://www.pixiv.net/ranking.php?mode=monthly'
+    #         pixiv_test = requests.get(i, headers=self.headers)
+    #         path = self.params_rank['mode'] + '/' + str(j) + '.jpg'
+    #         if not os.path.exists(self.params_rank['mode']):
+    #             os.mkdir(self.params_rank['mode'])
+    #         with open(path, 'wb') as f:
+    #             f.write(pixiv_test.content)
+    #             j += 1
+    #             print(f'图片{i}正在保存...')
+
+    def download(self):  # 多线程下载
+        while True:
+            self.glock.acquire()  # 加锁
+            if len(self.list_url) == 0:
+                self.glock.release()  # 释放锁
+                break
+            else:
+                if not os.path.exists('pixiv_imgs'):  # 创建文件夹
+                    os.mkdir('pixiv_imgs')
+                url = self.list_url.pop(0)  # 提取列表第一个URL并删除
+                self.glock.release()  # 释放锁
+                # 修改文件名
+                fname = url.replace('https://i.pximg.net/img-original/img/', '').replace('/', '_')
+                path = 'pixiv_imgs/' + fname
+                if os.path.exists(path):
+                    print(f'图片{fname}已存在， 跳过')
+                    continue
+                pixiv_img = requests.get(url, headers=self.headers)
+                with open(path, 'wb') as f:
+                    f.write(pixiv_img.content)
+                    print(f'图片{fname}正在保存...')
+                    self.i += 1
 
 
-if __name__ == '__main__':
-    # try:
-    #     task = os.sys.argv[1]
-    # except:
-    #     task = None
-    p = pixiv()
-    p.get_item("https://www.pixiv.net/ranking.php?mode=male")
-    p.multi_data(max=25)
+def main():
+    print('##————Pixiv————##')
+    moder = input('请输入排行榜的时间（日:1/周:2/月:3）:')
+    pn = input('请输入你想要多少页(50/页):')
+
+    pixivc = Pixiv(moder, pn)
+    pixivc.id_get()
+
+    for j in range(3):
+        urlget = threading.Thread(target=pixivc.url_get())
+        urlget.start()
+
+    for i in range(3):
+        download = threading.Thread(target=pixivc.download())
+        download.start()
+    print(f'爬取结束,共保存{pixivc.i - 1}张图片')
+
+
+if __name__ == "__main__":
+    main()
